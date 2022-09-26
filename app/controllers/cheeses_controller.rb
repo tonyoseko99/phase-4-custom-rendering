c3caf74ad9371817a4027afd6858f0ecd59235e0
Tonny Oseko <tonnyoseko0@gmail.com>
2022-09-26 23:58:26 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,22 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  end
+
+  # get cheeses summary that renders name and price
+  def summary
+    "#{name}: $#{price}"
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    render json: { error: 'Cheese not found' }
+  end
   end
 
 end
